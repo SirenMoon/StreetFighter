fe0e26748c86b06b3279033ddfae31e9c1cb1bd6
meira <garnetz25@hushmail.com>
2014-05-06 15:18:24 -0500

    commit 3
    audio, logo, placement, background etc.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
+  });
+  
+  $('.ryu').mouseleave(function() {
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-still').show();
-  })
-  .mousedown(function() {
+  });
+
+  $('.ryu').mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
-    .animate(
+    .animate (
       {'left': '300px'},
       500,
       function() {
@@ -20,15 +25,35 @@ $(document).ready(function() {
         $(this).css('left', '-212px');
       }
     );
-  })
-  .mouseup(function() {
+  });
+
+  $('.ryu').mouseup(function() {
     $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
   });
-});
-  
-function playHadouken () {
+
+  function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+  $('body').keydown(function(event){
+    if(event.keyCode == 88){
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ruy-throwing').hide();
+      $('.ryu-cool').show();
+    }
+  });
+
+  $('body').keyup(function(event){
+    if(event.keyCode == 88){
+      $('.ryu-cool').hide();
+      $('.ryu-still').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-ready').show();
+    }
+  });
+});
